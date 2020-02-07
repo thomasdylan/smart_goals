d778a641799f022a83b16d99f33691a090849272
Dylan Thomas <thomasdylan76@gmail.com>
2020-02-06 17:16:41 -0700

    Partial recovery of styling.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import NavBar from "./components/NavBar";
-import {useAuth0} from "./react-auth0-spa";
-import {Router, Route, Switch} from "react-router-dom";
+import { useAuth0 } from "./react-auth0-spa";
+import { Router, Route, Switch } from "react-router-dom";
 import Home from "./views/Home";
 import Profile from "./views/Profile";
 import Landing from "./views/Landing";
@@ -10,12 +10,12 @@ import history from "./utils/history";
 import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-    const {loading} = useAuth0();
+    const { loading } = useAuth0();
 
     if (loading) {
         return (
             <div className="container m-auto loading">
-            <div className="spinner-border text-primary loading" style={{ "width": "10rem", "height": "10rem" }} role="status">
+                <div className="spinner-border text-primary loading" style={{ "width": "10rem", "height": "10rem" }} role="status">
                     <span className="sr-only">Loading...</span>
                 </div>
             </div>
@@ -25,12 +25,12 @@ function App() {
         <div className="App">
             <Router history={history}>
                 <header>
-                    <NavBar/>
+                    <NavBar />
                 </header>
                 <Switch>
-                    <Route path="/" exact component={Landing}/>
-                    <PrivateRoute path="/profile" component={Profile}/>
-                    <PrivateRoute path="/home" component={Home}/>
+                    <Route path="/" exact component={Landing} />
+                    <PrivateRoute path="/profile" component={Profile} />
+                    <PrivateRoute path="/home" component={Home} />
                 </Switch>
             </Router>
         </div>
--- client/src/components/GoalList.js
+++ client/src/components/GoalList.js
@@ -10,35 +10,35 @@ export default function GoalList(props) {
     const { user } = useAuth0();
     const [userGoals, setUserGoals] = useState([]);
     const [goalUser] = useState(user.sub);
-    
+
     useEffect(() => {
         console.log(props.newGoalAdded);
         props.resetNewGoal(false);
         API.getGoal(goalUser)
-        .then(res => {
-            setUserGoals(res.data);
-        });
+            .then(res => {
+                setUserGoals(res.data);
+            });
     }, [props.newGoalAdded]);
-    
+
     console.log("userGoals: ", userGoals);
     return (
         <div>
             <Row sm="12">
                 {userGoals.map(goal => {
-                    return(
-                    <Card body className="goal-layout">
-                            <CircularProgressbar 
-                                circleRatio={0.75} 
-                                value={((goal.amount / goal.goalAmount).toFixed(2)) * 100} 
+                    return (
+                        <Card body className="goal-layout">
+                            <CircularProgressbar
+                                circleRatio={0.75}
+                                value={((goal.amount / goal.goalAmount).toFixed(2)) * 100}
                                 text={`${((goal.amount / goal.goalAmount).toFixed(2)) * 100}%`}
                                 styles={buildStyles({
                                     rotation: 1 / 2 + 1 / 8,
                                     strokeLinecap: "butt",
                                     trailColor: "#eee"
                                 })}
-                                />
-                        <CardTitle>{goal.title}</CardTitle>
-                    </Card>
+                            />
+                            <CardTitle>{goal.title}</CardTitle>
+                        </Card>
                     )
                 })}
             </Row>
